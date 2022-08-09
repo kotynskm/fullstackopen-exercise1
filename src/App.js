f714fadd930192dbe947a2a67b72d326860185cf
Kailey <k.kotynski@gmail.com>
2022-08-09 14:46:35 -0700

    Changed variables in App component to an object, modified return statement.

--- src/App.js
+++ src/App.js
@@ -30,21 +30,31 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <Fragment>
-    <Header course={course} />
-    <Content part={part1} exercises={exercises1} />
-    <Content part={part2} exercises={exercises2} />
-    <Content part={part3} exercises={exercises3} />
-    <Total total={exercises1 + exercises2 + exercises3} />
+    <Header course={course.name} />
+    <Content part={course.parts[0].name} exercises={course.parts[0].exercises} />
+    <Content part={course.parts[1].name} exercises={course.parts[1].exercises} />
+    <Content part={course.parts[2].name} exercises={course.parts[2].exercises} />
+    <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
     </Fragment>
   )
 }
